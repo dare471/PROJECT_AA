74b8d211ef60fc3b43c1c55626e14243096b22cf
Gaziz <maks.mafia00@mail.ru>
2022-12-11 13:49:48 +0600

    build(shared/routes)

--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -3,11 +3,13 @@ import { getRoute } from '~src/shared/lib/route'
 export const routesPath = {
 	home: 'home',
 	map: 'map',
+	mapPlayGround: 'map-play-ground',
 	error: 'error'
 }
 
 export const routes = {
 	home: getRoute({ path: routesPath.home }),
 	map: getRoute({ path: routesPath.map }),
+	mapPlayGround: getRoute({ path: routesPath.mapPlayGround }),
 	error: getRoute({ path: routesPath.error })
 }
