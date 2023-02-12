16a1999c0c395cef4d118717983a030fe8ad4f37
Gaziz <maks.mafia00@mail.ru>
2023-02-12 15:17:44 +0600

    refactor(header): widgets/header

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,29 +1,18 @@
-import {
-	Box,
-	Button,
-	ButtonGroup,
-	Container,
-	Flex,
-	Grid,
-	Icon,
-	IconButton,
-	Image,
-	Link,
-	LinkBox,
-} from '@chakra-ui/react'
+import { Box, Button, ButtonGroup, Container, Flex, Grid, Icon, Image, LinkBox } from '@chakra-ui/react'
 import { BsMap } from 'react-icons/bs'
 import { useNavigate } from 'react-router'
 
-import { SessionPanel } from '~src/entities/session'
+import { SessionPanel, ShowOnly } from '~src/entities/session'
 
 import { LogoImage } from '~src/shared/assets'
+import { zIndices } from '~src/shared/lib'
 import { routes } from '~src/shared/routes'
 
 export function Header() {
 	const navigate = useNavigate()
 
 	return (
-		<Box bgColor='whiteAlpha.100' boxShadow='md'>
+		<Box bgColor='whiteAlpha.100' boxShadow='md' position='relative' zIndex={zIndices.header}>
 			<Container maxW='container.xl' h='4rem'>
 				<Grid h='full' templateColumns={'1fr 1fr 1fr'}>
 					<Flex justify='start' align='center'>
@@ -33,16 +22,23 @@ export function Header() {
 					</Flex>
 					<Flex justify='center' align='center'>
 						{/* <ButtonGroup>
-							
 						</ButtonGroup> */}
 					</Flex>
 					<Flex justify='end' align='center'>
-						<ButtonGroup alignItems='center'>
-							<Button variant='ghost' colorScheme='blue' onClick={() => navigate(routes.map())}>
-								<Icon as={BsMap} />
+						<ShowOnly when='guest'>
+							<Button colorScheme='blue' onClick={() => navigate(routes.signIn())}>
+								Войти
 							</Button>
-							<SessionPanel />
-						</ButtonGroup>
+						</ShowOnly>
+
+						<ShowOnly when='auth'>
+							<ButtonGroup alignItems='center'>
+								<Button variant='ghost' colorScheme='blue' onClick={() => navigate(routes.map())}>
+									<Icon as={BsMap} />
+								</Button>
+								<SessionPanel />
+							</ButtonGroup>
+						</ShowOnly>
 					</Flex>
 				</Grid>
 			</Container>
