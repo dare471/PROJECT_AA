4640d1193f3d7065d023c26d0b9101e0e8f80766
Gaziz <maks.mafia00@mail.ru>
2023-02-12 10:45:51 +0600

    build(map): pages/map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router'
 
 import { lazy } from '~src/shared/lib'
+import { paths } from '~src/shared/routes'
 
 const HomePage = lazy(() => import('./home'), 'HomePage')
+const MapPage = lazy(() => import('./map'), 'MapPage')
 
 export function Pages() {
 	return (
 		<Routes>
-			<Route path='/' element={<HomePage />}></Route>
+			<Route path={paths.home} element={<HomePage />} />
+			<Route path={paths.map} element={<MapPage />}></Route>
 		</Routes>
 	)
 }
