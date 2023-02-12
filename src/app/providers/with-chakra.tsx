27f77b35291c499026f958a0b9f62fde69fb1da6
Gaziz <maks.mafia00@mail.ru>
2023-02-12 10:48:02 +0600

    build(providers): app/providers

--- src/app/providers/with-chakra.tsx
+++ src/app/providers/with-chakra.tsx
@@ -3,4 +3,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '~src/shared/lib'
 
 export const withChakra = (component: () => React.ReactNode) => () =>
-	<ChakraProvider theme={theme}>{component()}</ChakraProvider>
+	(
+		<ChakraProvider resetCSS theme={theme}>
+			{component()}
+		</ChakraProvider>
+	)
--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react'
-import { Center, Spinner } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
+
+import { FullSpinner } from '~src/shared/ui'
 
 export const withRouter = (component: () => React.ReactNode) => () =>
 	(
 		<BrowserRouter>
-			<React.Suspense
-				fallback={
-					<Center h='100vh'>
-						<Spinner size='lg' color='blue.500' />
-					</Center>
-				}
-			>
-				{component()}
-			</React.Suspense>
+			<React.Suspense fallback={<FullSpinner />}>{component()}</React.Suspense>
 		</BrowserRouter>
 	)
