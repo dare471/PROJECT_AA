fab7011a84247c3274e30cfeee3cad5b05360349
Gaziz <maks.mafia00@mail.ru>
2023-02-12 15:13:22 +0600

    refactor(map): pages/map

---
+++ src/pages/map/model.ts
@@ -0,0 +1,4 @@
+import type L from 'leaflet'
+
+export const CENTER = [48.356, 66.687] as L.LatLngTuple
+export const ZOOM = 5
--- src/pages/map/page.tsx
+++ src/pages/map/page.tsx
@@ -1,3 +1,26 @@
+import { Box } from '@chakra-ui/react'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+import { Header } from '~src/widgets/header'
+
+import { zIndices } from '~src/shared/lib'
+
+import * as model from './model'
+
+export const MAP_ATTRIBUTION =
+	'&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+export const MAP_URL = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'
+
 export function MapPage() {
-	return <></>
+	return (
+		<Box>
+			<Header />
+			<Box minH='calc(100vh - 4rem)'>
+				<Box as={MapContainer} h='full' minH='inherit' zIndex={zIndices.map} center={model.CENTER} zoom={model.ZOOM}>
+					<TileLayer attribution={MAP_ATTRIBUTION} url={MAP_URL} />
+				</Box>
+			</Box>
+		</Box>
+	)
 }
