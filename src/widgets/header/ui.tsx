161cc434ada60cacbde88906ab452b9b4d0a8cef
Gaziz <maks.mafia00@mail.ru>
2023-02-17 01:46:16 +0600

    refactor(header): refactor ui

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,18 +1,31 @@
-import { Box, Button, ButtonGroup, Container, Flex, Grid, Icon, Image, LinkBox } from '@chakra-ui/react'
+import {
+	Box,
+	Button,
+	ButtonGroup,
+	type ButtonProps,
+	Container,
+	Flex,
+	Grid,
+	Icon,
+	Image,
+	LinkBox,
+	Text,
+} from '@chakra-ui/react'
+import { useUnit } from 'effector-react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { BsMap } from 'react-icons/bs'
 import { useNavigate } from 'react-router'
 
-import { SessionPanel, ShowOnly } from '~src/entities/session'
+import { sessionModel, SessionPanel, ShowOnly } from '~src/entities/session'
 
 import { LogoImage } from '~src/shared/assets'
-import { zIndices } from '~src/shared/lib'
 import { routes } from '~src/shared/routes'
 
 export function Header() {
 	const navigate = useNavigate()
 
 	return (
-		<Box bgColor='whiteAlpha.100' boxShadow='md' position='relative' zIndex={zIndices.header}>
+		<Box bgColor='whiteAlpha.100' boxShadow='md' position='relative' zIndex='fixed'>
 			<Container maxW='container.xl' h='4rem'>
 				<Grid h='full' templateColumns={'1fr 1fr 1fr'}>
 					<Flex justify='start' align='center'>
@@ -36,6 +49,8 @@ export function Header() {
 								<Button variant='ghost' colorScheme='blue' onClick={() => navigate(routes.map())}>
 									<Icon as={BsMap} />
 								</Button>
+
+								<ClientCardIcon onClick={() => navigate(routes.favoriteClientCart())} />
 								<SessionPanel />
 							</ButtonGroup>
 						</ShowOnly>
@@ -43,5 +58,30 @@ export function Header() {
 				</Grid>
 			</Container>
 		</Box>
+	)
+}
+
+function ClientCardIcon(props: ButtonProps) {
+	const [session] = useUnit([sessionModel.$session])
+
+	if (!session) return null
+
+	return (
+		<Button colorScheme='blue' position='relative' variant='ghost' {...props}>
+			<Icon as={AiOutlineShoppingCart} boxSize='7' color='blue.500' />
+			<Text
+				position='absolute'
+				top='50%'
+				left='50%'
+				color='white'
+				transform='translateY(-100%) translateX(-100%)'
+				fontSize='x-small'
+				p='1'
+				bgColor='blue.500'
+				rounded='full'
+			>
+				{session.favoriteClients.length > 99 ? '99+' : session.favoriteClients.length}
+			</Text>
+		</Button>
 	)
 }
