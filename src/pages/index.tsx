a368110a2cbd40c295c171ce47a1625f18fea059
Gaziz <maks.mafia00@mail.ru>
2023-02-17 01:47:03 +0600

    refactor(pages)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,60 @@
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
+
+import { AuthProvider, GuestProvider } from '~src/entities/session'
 
 import { lazy } from '~src/shared/lib'
-import { paths } from '~src/shared/routes'
+import { paths, routes } from '~src/shared/routes'
 
 const HomePage = lazy(() => import('./home'), 'HomePage')
+const SignInPage = lazy(() => import('./sign-in'), 'SignInPage')
 const MapPage = lazy(() => import('./map'), 'MapPage')
+const FavoriteClientCartPage = lazy(() => import('./favorite-client-cart'), 'FavoriteClientCartPage')
+const UserPublicProfilePage = lazy(() => import('./user-public-profile'), 'UserPublicProfilePage')
+const ErrorPage = lazy(() => import('./error'), 'ErrorPage')
 
 export function Pages() {
 	return (
 		<Routes>
 			<Route path={paths.home} element={<HomePage />} />
-			<Route path={paths.map} element={<MapPage />}></Route>
+			<Route
+				path={paths.signIn}
+				element={
+					<GuestProvider>
+						<SignInPage />
+					</GuestProvider>
+				}
+			/>
+			<Route
+				path={paths.map}
+				element={
+					<AuthProvider>
+						<MapPage />
+					</AuthProvider>
+				}
+			/>
+
+			<Route
+				path={paths.favoriteClientCart}
+				element={
+					<AuthProvider>
+						<FavoriteClientCartPage />
+					</AuthProvider>
+				}
+			/>
+
+			<Route
+				path={paths.userPublicProfile}
+				element={
+					<AuthProvider>
+						<UserPublicProfilePage />
+					</AuthProvider>
+				}
+			/>
+
+			<Route path={paths.userSettingsProfile} element={<AuthProvider>{/* <UserSettingsProfile /> */}</AuthProvider>} />
+
+			<Route path='error' element={<ErrorPage />} />
+			<Route path='*' element={<Navigate to={routes.error()} />} />
 		</Routes>
 	)
 }
